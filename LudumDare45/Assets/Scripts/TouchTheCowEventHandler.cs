2b7797410bfb3d9de5723a909a0404b8a350aa0a
QuanMao <2541673770@qq.com>
2019-10-05 19:49:04 +0800

    Delete FungusExample

---
+++ LudumDare45/Assets/Scripts/TouchTheCowEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+[EventHandlerInfo("MyHandler", "TouchTheCow", "Set the touchTheCow to control blocks.")]
+public class TouchTheCowEventHandler : EventHandler
+{
+    public bool touchTheCow; // static
+
+    private void Update()
+    {
+        if (touchTheCow)
+        {
+            ExecuteBlock();
+        }
+    }
+}
