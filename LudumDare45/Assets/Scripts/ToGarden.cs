aec643b4b91e775104337dd0e2dfb6dfec7384a7
QuanMao <2541673770@qq.com>
2019-10-06 18:34:08 +0800

    Add Garden

---
+++ LudumDare45/Assets/Scripts/ToGarden.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToGarden : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Vector3 nextPosition = new Vector3(2,7,0);
+            collision.transform.position = nextPosition;
+
+            cam2.SetActive(true);
+            cam1.SetActive(false);
+        }
+    }
+}
---
+++ LudumDare45/Assets/Scripts/ToHome.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToHome : MonoBehaviour
+{
+    public GameObject cam1;
+    public GameObject cam2;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            Vector3 nextPosition = new Vector3(0, 4, 0);
+            collision.transform.position = nextPosition;
+
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+        }
+    }
+}
