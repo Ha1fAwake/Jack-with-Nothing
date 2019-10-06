59e3270a43c09ade36789d3505556fd00202f357
LittleWhite <2798497380@qq.com>
2019-10-06 18:17:42 +0800

    修改AI代码

--- LudumDare45/Assets/LudumDare/Scripts/BossAi_1.cs
+++ LudumDare45/Assets/LudumDare/Scripts/BossAi_1.cs
@@ -1,11 +1,20 @@
 using JetBrains.Annotations;
 using ReadyGamerOne.Const;
 using UnityEngine;
+using ReadyGamerOne.Common;
 
-namespace LudumDare.Scripts
-{
-    public class BossAi_1 : MonoBehaviour
-    {
+namespace LudumDare.Scripts {
+    public class BossAi_1 : MonoBehaviour {
         public ConstStringChooser messageWhenStateEnd;
+        public GameObject Bullet;
+        public float Speed;
+        public int BulletNumm = 20;
+        public static int DeadBullet = 0;
+
+
+
+        private void AnnounceStateEnd() {
+            CEventCenter.BroadMessage(messageWhenStateEnd.StringValue);
+        }
     }
 }
--- LudumDare45/Assets/Scripts/RandomFly.cs
+++ LudumDare45/Assets/Scripts/RandomFly.cs
@@ -1,6 +1,10 @@
 ﻿using UnityEngine;
+using LudumDare.Scripts;
+using System;
 
 public class RandomFly : MonoBehaviour {
+
+    public static event Action<Collision2D> onColliderEnter;
 
     public int CollisionNum = 10;
     public float Speed = 5;
@@ -8,14 +12,18 @@ public class RandomFly : MonoBehaviour {
 
     private void Start() {
         Vector3 SpeedVec = new Vector3(0, 0, 0);
-        SpeedVec.x = Random.Range(-1.0f, 1.0f);
-        SpeedVec.y = Random.Range(-1.0f, 0.0f);
+        SpeedVec.x = UnityEngine.Random.Range(-1.0f, 1.0f);
+        SpeedVec.y = UnityEngine.Random.Range(-1.0f, 0.0f);
         SpeedVec = SpeedVec.normalized * Speed;
         GetComponent<Rigidbody2D>().velocity = SpeedVec;
     }
 
     private void OnCollisionEnter2D(Collision2D c) {
         CountCollision++;
-        if (CountCollision == Speed) Destroy(gameObject);
+        onColliderEnter?.Invoke(c);
+        if (CountCollision == CollisionNum) {
+            BossAi_1.DeadBullet++;
+            Destroy(gameObject);
+        }
     }
 }
