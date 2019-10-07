d74d8292ac971060e6cac5735744a94b0a4dd1e0
QuanMao <2541673770@qq.com>
2019-10-07 16:16:50 +0800

    Add Audios and Effects and Sprites Resources

---
+++ LudumDare45/Assets/Scripts/ContactWithMerchant.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Fungus;
+
+public class ContactWithMerchant : MonoBehaviour
+{
+    public Flowchart flow;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        flow.ExecuteBlock("JackWithMerchant");
+    }
+}
